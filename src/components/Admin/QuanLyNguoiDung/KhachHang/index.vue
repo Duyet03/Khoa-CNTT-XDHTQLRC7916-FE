9544d92aae80e30beaed74184eb9ca463736a03b
Minh Tùng Hồ <tungho.nth@gmail.com>
2025-05-11 16:29:53 +0700

    validate khach hang

--- src/components/Admin/QuanLyNguoiDung/KhachHang/index.vue
+++ src/components/Admin/QuanLyNguoiDung/KhachHang/index.vue
@@ -90,26 +90,49 @@
                             <div class="col-md-6">
                                 <label class="form-label required">Tên khách hàng</label>
                                 <input v-model="khach_hang_create.ten_khach_hang" type="text" class="form-control" />
+                                <div v-if="errors.ten_khach_hang" class="text-danger"
+                                    style="font-size: 15px; margin-top: 2px;">
+                                    {{ errors.ten_khach_hang }}
+                                </div>
                             </div>
                             <div class="col-md-6">
                                 <label class="form-label required">Email</label>
                                 <input v-model="khach_hang_create.email" type="email" class="form-control" />
+                                <div v-if="errors.email" class="text-danger" style="font-size: 15px; margin-top: 2px;">
+                                    {{ errors.email }}
+                                </div>
                             </div>
                             <div class="col-md-6">
                                 <label class="form-label">Số điện thoại</label>
                                 <input v-model="khach_hang_create.so_dien_thoai" type="text" class="form-control" />
+                                <div v-if="errors.so_dien_thoai" class="text-danger"
+                                    style="font-size: 15px; margin-top: 2px;">
+                                    {{ errors.so_dien_thoai }}
+                                </div>
                             </div>
                             <div class="col-md-6">
                                 <label class="form-label">Ngày sinh</label>
                                 <input v-model="khach_hang_create.ngay_sinh" type="date" class="form-control" />
+                                <div v-if="errors.ngay_sinh" class="text-danger"
+                                    style="font-size: 15px; margin-top: 2px;">
+                                    {{ errors.ngay_sinh }}
+                                </div>
                             </div>
                             <div class="col-md-6">
                                 <label class="form-label required">Mật khẩu</label>
                                 <input v-model="khach_hang_create.password" type="password" class="form-control" />
+                                <div v-if="errors.password" class="text-danger"
+                                    style="font-size: 15px; margin-top: 2px;">
+                                    {{ errors.password }}
+                                </div>
                             </div>
                             <div class="col-md-6">
                                 <label class="form-label required">Nhập lại mật khẩu</label>
                                 <input v-model="khach_hang_create.re_password" type="password" class="form-control" />
+                                <div v-if="errors.re_password" class="text-danger"
+                                    style="font-size: 15px; margin-top: 2px;">
+                                    {{ errors.re_password }}
+                                </div>
                             </div>
                             <div class="col-md-6">
                                 <label class="form-label">Tình trạng</label>
@@ -131,7 +154,7 @@
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
                             <i class="bx bx-x me-1"></i>Đóng
                         </button>
-                        <button @click="themKhachHang()" type="button" class="btn btn-primary" data-bs-dismiss="modal">
+                        <button @click="themKhachHang()" type="button" class="btn btn-primary">
                             <i class="bx bx-check me-1"></i>Thêm mới
                         </button>
                     </div>
@@ -140,10 +163,11 @@
         </div>
 
         <!-- Update Modal -->
-        <div class="modal fade" id="updateModal" tabindex="-1">
+        <!-- Modal Update Khách Hàng -->
+        <div class="modal fade" id="updateKhachHangModal" tabindex="-1">
             <div class="modal-dialog modal-lg">
                 <div class="modal-content">
-                    <div class="modal-header bg-info text-white">
+                    <div class="modal-header bg-primary text-white">
                         <h5 class="modal-title">Cập Nhật Khách Hàng</h5>
                         <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal"></button>
                     </div>
@@ -152,18 +176,34 @@
                             <div class="col-md-6">
                                 <label class="form-label required">Tên khách hàng</label>
                                 <input v-model="khach_hang_update.ten_khach_hang" type="text" class="form-control" />
+                                <div v-if="errors_update.ten_khach_hang" class="text-danger"
+                                    style="font-size: 15px; margin-top: 2px;">
+                                    {{ errors_update.ten_khach_hang }}
+                                </div>
                             </div>
                             <div class="col-md-6">
                                 <label class="form-label required">Email</label>
                                 <input v-model="khach_hang_update.email" type="email" class="form-control" />
+                                <div v-if="errors_update.email" class="text-danger"
+                                    style="font-size: 15px; margin-top: 2px;">
+                                    {{ errors_update.email }}
+                                </div>
                             </div>
                             <div class="col-md-6">
                                 <label class="form-label">Số điện thoại</label>
                                 <input v-model="khach_hang_update.so_dien_thoai" type="text" class="form-control" />
+                                <div v-if="errors_update.so_dien_thoai" class="text-danger"
+                                    style="font-size: 15px; margin-top: 2px;">
+                                    {{ errors_update.so_dien_thoai }}
+                                </div>
                             </div>
                             <div class="col-md-6">
                                 <label class="form-label">Ngày sinh</label>
                                 <input v-model="khach_hang_update.ngay_sinh" type="date" class="form-control" />
+                                <div v-if="errors_update.ngay_sinh" class="text-danger"
+                                    style="font-size: 15px; margin-top: 2px;">
+                                    {{ errors_update.ngay_sinh }}
+                                </div>
                             </div>
                             <div class="col-md-6">
                                 <label class="form-label">Tình trạng</label>
@@ -185,8 +225,8 @@
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
                             <i class="bx bx-x me-1"></i>Đóng
                         </button>
-                        <button @click="capNhatKhachHang()" class="btn btn-info" data-bs-dismiss="modal">
-                            <i class="bx bx-save me-1"></i>Lưu thay đổi
+                        <button @click="capNhatKhachHang()" type="button" class="btn btn-primary">
+                            <i class="bx bx-check me-1"></i>Cập nhật
                         </button>
                     </div>
                 </div>
@@ -239,6 +279,14 @@ export default {
                 is_block: 0,
                 tinh_trang: 1,
             },
+            errors: {
+                ten_khach_hang: '',
+                email: '',
+                so_dien_thoai: '',
+                ngay_sinh: '',
+                password: '',
+                re_password: ''
+            },
             khach_hang_update: {
                 ten_khach_hang: '',
                 email: '',
@@ -246,6 +294,12 @@ export default {
                 ngay_sinh: '',
                 is_block: 0,
                 tinh_trang: 1,
+            },
+            errors_update: {
+                ten_khach_hang: '',
+                email: '',
+                so_dien_thoai: '',
+                ngay_sinh: ''
             },
             id_can_xoa: '',
         }
@@ -256,6 +310,80 @@ export default {
     },
 
     methods: {
+        validateForm() {
+            let isValid = true;
+            this.errors = {
+                ten_khach_hang: '',
+                email: '',
+                so_dien_thoai: '',
+                ngay_sinh: '',
+                password: '',
+                re_password: ''
+            };
+
+            // Tên khách hàng
+            if (!this.khach_hang_create.ten_khach_hang) {
+                this.errors.ten_khach_hang = 'Tên khách hàng không được để trống!';
+                isValid = false;
+            }
+
+            // Email
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!this.khach_hang_create.email) {
+                this.errors.email = 'Email không được để trống!';
+                isValid = false;
+            } else if (!emailRegex.test(this.khach_hang_create.email)) {
+                this.errors.email = 'Email không đúng định dạng!';
+                isValid = false;
+            }
+
+            // Số điện thoại (nếu nhập thì phải đúng định dạng)
+            const phoneRegex = /^([0-9\s\-\+\(\)]*)$/;
+            if (this.khach_hang_create.so_dien_thoai) {
+                if (!phoneRegex.test(this.khach_hang_create.so_dien_thoai)) {
+                    this.errors.so_dien_thoai = 'Số điện thoại không đúng định dạng!';
+                    isValid = false;
+                } else if (
+                    this.khach_hang_create.so_dien_thoai.length < 10 ||
+                    this.khach_hang_create.so_dien_thoai.length > 11
+                ) {
+                    this.errors.so_dien_thoai = 'Số điện thoại phải từ 10-11 số!';
+                    isValid = false;
+                }
+            }
+
+            // Ngày sinh (nếu nhập thì không được lớn hơn ngày hiện tại)
+            if (this.khach_hang_create.ngay_sinh) {
+                const today = new Date();
+                const birthDate = new Date(this.khach_hang_create.ngay_sinh);
+                if (birthDate > today) {
+                    this.errors.ngay_sinh = 'Ngày sinh không được lớn hơn ngày hiện tại!';
+                    isValid = false;
+                }
+            }
+
+            // Mật khẩu
+            if (!this.khach_hang_create.password) {
+                this.errors.password = 'Mật khẩu không được để trống!';
+                isValid = false;
+            } else if (this.khach_hang_create.password.length < 6) {
+                this.errors.password = 'Mật khẩu phải ít nhất 6 ký tự!';
+                isValid = false;
+            }
+
+            // Nhập lại mật khẩu
+            if (!this.khach_hang_create.re_password) {
+                this.errors.re_password = 'Vui lòng nhập lại mật khẩu!';
+                isValid = false;
+            } else if (this.khach_hang_create.password !== this.khach_hang_create.re_password) {
+                this.errors.re_password = 'Mật khẩu nhập lại không khớp!';
+                isValid = false;
+            }
+
+            return isValid;
+        },
+
+
         layKhachHang() {
             baseRequest.get("khach-hang/data")
                 .then((res) => {
@@ -264,49 +392,161 @@ export default {
         },
 
         themKhachHang() {
-            if (!this.khach_hang_create.password || !this.khach_hang_create.re_password) {
-                toaster.error("Vui lòng nhập đầy đủ mật khẩu!");
+            if (!this.validateForm()) {
+                // Nếu có lỗi thì không gửi lên BE
                 return;
             }
 
-            if (this.khach_hang_create.password !== this.khach_hang_create.re_password) {
-                toaster.error("Mật khẩu nhập lại không khớp!");
-                return;
-            }
-
+            // Nếu không có lỗi thì gửi lên BE
             baseRequest.post('khach-hang/create', this.khach_hang_create)
                 .then((res) => {
                     if (res.data.status) {
-                        toaster.success(res.data.message);
                         this.layKhachHang();
                         $("#taoPhongModal").modal('hide');
                         this.resetCreateForm();
                     } else {
-                        toaster.error(res.data.message);
+                        // Nếu BE trả về message chung
+                        if (res.data.message) {
+                            this.errors.ten_khach_hang = res.data.message;
+                        }
                     }
                 })
-
+                .catch((error) => {
+                    // Nếu BE trả về lỗi validation
+                    if (error.response && error.response.data.errors) {
+                        const backendErrors = error.response.data.errors;
+                        Object.keys(backendErrors).forEach(key => {
+                            if (this.errors.hasOwnProperty(key)) {
+                                this.errors[key] = backendErrors[key][0];
+                            }
+                        });
+                    }
+                });
         },
 
+        resetCreateForm() {
+            this.khach_hang_create = {
+                ten_khach_hang: '',
+                email: '',
+                so_dien_thoai: '',
+                ngay_sinh: '',
+                password: '',
+                re_password: '',
+                is_block: 0,
+                tinh_trang: 1
+            };
+            this.errors = {
+                ten_khach_hang: '',
+                email: '',
+                so_dien_thoai: '',
+                ngay_sinh: '',
+                password: '',
+                re_password: ''
+            };
+        },
         openUpdateModal(khachHang) {
             this.khach_hang_update = { ...khachHang };
-            const updateModal = new bootstrap.Modal(document.getElementById('updateModal'));
+            const updateModal = new bootstrap.Modal(document.getElementById('updateKhachHangModal'));
             updateModal.show();
         },
 
+        validateUpdateForm() {
+            let isValid = true;
+            this.errors_update = {
+                ten_khach_hang: '',
+                email: '',
+                so_dien_thoai: '',
+                ngay_sinh: ''
+            };
+
+            // Tên khách hàng
+            if (!this.khach_hang_update.ten_khach_hang) {
+                this.errors_update.ten_khach_hang = 'Tên khách hàng không được để trống!';
+                isValid = false;
+            }
+
+            // Email
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!this.khach_hang_update.email) {
+                this.errors_update.email = 'Email không được để trống!';
+                isValid = false;
+            } else if (!emailRegex.test(this.khach_hang_update.email)) {
+                this.errors_update.email = 'Email không đúng định dạng!';
+                isValid = false;
+            }
+
+            // Số điện thoại (nếu nhập thì phải đúng định dạng)
+            const phoneRegex = /^([0-9\s\-\+\(\)]*)$/;
+            if (this.khach_hang_update.so_dien_thoai) {
+                if (!phoneRegex.test(this.khach_hang_update.so_dien_thoai)) {
+                    this.errors_update.so_dien_thoai = 'Số điện thoại không đúng định dạng!';
+                    isValid = false;
+                } else if (
+                    this.khach_hang_update.so_dien_thoai.length < 10 ||
+                    this.khach_hang_update.so_dien_thoai.length > 11
+                ) {
+                    this.errors_update.so_dien_thoai = 'Số điện thoại phải từ 10-11 số!';
+                    isValid = false;
+                }
+            }
+
+            // Ngày sinh (nếu nhập thì không được lớn hơn ngày hiện tại)
+            if (this.khach_hang_update.ngay_sinh) {
+                const today = new Date();
+                const birthDate = new Date(this.khach_hang_update.ngay_sinh);
+                if (birthDate > today) {
+                    this.errors_update.ngay_sinh = 'Ngày sinh không được lớn hơn ngày hiện tại!';
+                    isValid = false;
+                }
+            }
+
+            return isValid;
+        },
+
+
         capNhatKhachHang() {
-            baseRequest.put("khach-hang/update", this.khach_hang_update)
+            if (!this.validateUpdateForm()) {
+                // Nếu có lỗi thì không gửi lên BE
+                return;
+            }
+
+            // Nếu không có lỗi thì gửi lên BE
+            baseRequest.put('khach-hang/update', this.khach_hang_update)
                 .then((res) => {
                     if (res.data.status) {
                         toaster.success(res.data.message);
                         this.layKhachHang();
-                        $("#updateModal").modal('hide');
+                        // Đóng modal đúng chuẩn Bootstrap 5
+                        const modalEl = document.getElementById('updateKhachHangModal');
+                        const modalInstance = bootstrap.Modal.getInstance(modalEl) || new bootstrap.Modal(modalEl);
+                        modalInstance.hide();
+                        this.resetUpdateForm();
                     } else {
-                        toaster.error(res.data.message);
+                        // Nếu BE trả về message chung
+                        if (res.data.message) {
+                            this.errors_update.ten_khach_hang = res.data.message;
+                        }
                     }
                 })
         },
 
+        resetUpdateForm() {
+            this.khach_hang_update = {
+                id: '',
+                ten_khach_hang: '',
+                email: '',
+                so_dien_thoai: '',
+                ngay_sinh: '',
+                is_block: 0,
+                tinh_trang: 1
+            };
+            this.errors_update = {
+                ten_khach_hang: '',
+                email: '',
+                so_dien_thoai: '',
+                ngay_sinh: ''
+            };
+        },
         openDeleteModal(khachHang) {
             this.id_can_xoa = khachHang.id;
             const deleteModal = new bootstrap.Modal(document.getElementById('deleteModal'));
